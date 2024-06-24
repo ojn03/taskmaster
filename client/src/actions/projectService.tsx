4a4a9dd99ac2f23ca3788ee41919c9734c85491e
OJisMe <olivierjohnnn@gmail.com>
2024-06-23 23:57:37 -0700

    build out services

--- client/src/actions/projectService.tsx
+++ client/src/actions/projectService.tsx
@@ -1,7 +1,21 @@
-"use server"
+"use server";
 
 import { base } from "@/actions/host";
 
 export async function getProjectMembers({ projid }: { projid: number }) {
   const res = await fetch(`${base}/projects/${projid}/users`);
+}
+
+export async function getProjectTickets({ projid }: { projid: number }) {
+  // add typing
+  const res = await fetch(`${base}/projects/${projid}/tickets`);
+}
+
+export async function getProjectInfo({
+  projid,
+}: {
+  projid: number;
+}): Promise<Project> {
+  const res = await fetch(`${base}/projects/${projid}`);
+  return await res.json();
 }
--- client/src/actions/ticketService.tsx
+++ client/src/actions/ticketService.tsx
@@ -2,11 +2,34 @@
 // import typia from "typia";
 import { base } from "@/actions/host";
 
-
 export async function getTicket({ tick_id }: { tick_id: number }) {
   return await getAssert<Ticket>(`ticket/${tick_id}`);
 }
 
 async function getAssert<T>(route: string) {
   return await fetch(`${base}/${route}`).then((res) => res.json());
+}
+
+export async function updateTicket({
+  tick_id,
+  ticket_title,
+  ticket_description,
+  ticket_progress,
+}: {
+  tick_id: number;
+  ticket_title?: string;
+  ticket_description?: string;
+  ticket_progress?: number;
+}) {
+  return await fetch(`${base}/ticket/${tick_id}`, {
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      ticket_title,
+      ticket_description,
+      ticket_progress,
+    }),
+  });
 }
